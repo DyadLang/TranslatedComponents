524f63299ef9159f9fbc2268dc64121b9e6426ee
Venkateshprasad Bhat <32921645+ven-k@users.noreply.github.com>
2025-08-08 12:49:59 +0000

    feat: init TranslatedComponents module

---
+++ src/TranslatedComponents.jl
@@ -0,0 +1,8 @@
+module TranslatedComponents
+
+include("../generated/types.jl")
+include("../generated/definitions.jl")
+include("../generated/experiments.jl")
+include("../generated/precompilation.jl")
+
+end # module TranslatedComponents
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+
+using TranslatedComponents
+using Test
+
+include("../generated/tests.jl")
