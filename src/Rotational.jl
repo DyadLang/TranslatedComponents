15302d3f2edada9efc53d4a30bf14962f24fc5b4
Venkateshprasad Bhat <32921645+ven-k@users.noreply.github.com>
2025-08-11 10:43:53 +0000

    feat: add Rotational components
    These models are translated from ModelicaStandardLibrary/Mechanics/Rotational to Dyad using the Dyad AI workflow

---
+++ src/Rotational.jl
@@ -0,0 +1,8 @@
+module Rotational
+
+include("../generated/Rotational/types.jl")
+include("../generated/Rotational/definitions.jl")
+include("../generated/Rotational/experiments.jl")
+include("../generated/Rotational/precompilation.jl")
+
+end # module Rotational
--- src/TranslatedComponents.jl
+++ src/TranslatedComponents.jl
@@ -4,4 +4,6 @@ include("utils.jl")
 
 include("../generated/module.jl")
 
+include("Rotational.jl")
+
 end # module TranslatedComponents
